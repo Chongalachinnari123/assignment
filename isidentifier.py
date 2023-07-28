75e357f85cb659d691d2a742ccc842695eafca58
Chinnari Chongala <chongalachinnari.marolix@gamil.com>
2023-07-28 15:24:14 +0530

    solution

---
+++ isidentifier.py
@@ -0,0 +1,24 @@
+#isidentifier()
+
+string = "Myfolder"
+result = string.isidentifier()
+print(result)
+
+#2
+string = "Demo002"
+result = string.isidentifier()
+print(result)
+
+#3
+string = "2bring"
+result = string.isidentifier()
+print(result)
+
+#4
+string = "My demo"
+result = string.isidentifier()
+print(result)
+
+string = "chinnari"
+result = string.isidentifier()
+print(result)
