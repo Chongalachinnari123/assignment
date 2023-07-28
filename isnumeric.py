cdcf71a3b4ae26822b2e28d6a241be7bc86cda26
Chinnari Chongala <chongalachinnari.marolix@gamil.com>
2023-07-28 15:28:34 +0530

    solution

---
+++ isnumeric.py
@@ -0,0 +1,25 @@
+#isnumeric()
+
+#1
+string = "23456"
+result = string.isnumeric()
+print(result)
+
+#2
+string = "1234"
+result = string.isnumeric()
+print(result)
+
+#3
+string = "123abc"
+result = string.isnumeric()
+print(result)
+#4
+string = "abc"
+result = string.isnumeric()
+print(result)
+
+#5
+string = "1230"
+result = string.isnumeric()
+print(result)
