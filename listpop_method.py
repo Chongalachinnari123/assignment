370026fb7bb1160698df39de975d6b8876472a01
Chinnari Chongala <chongalachinnari.marolix@gamil.com>
2023-07-31 10:18:20 +0530

    solution

--- listpop_method.py
+++ listpop_method.py
@@ -1,1 +1,23 @@
 #pop()
+#1
+string = [20,30,40,50,70]
+result = string.pop()
+print(result)
+#2
+string = ["chinnari","anusha","praveen","sai"]
+string.pop()
+print(string)
+
+#3
+string = [1,3,2,4,5,6,7]
+result = string.pop()
+print(result)
+
+#4
+string = ["chinnari","dhananjaya naidu","chandarakala","srinu"]
+list = []
+for item in string:
+    item.pop()
+print(item)
+#5
+
