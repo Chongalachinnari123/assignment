ae466f3dbadf8e756ee6063edc14c2a1763b453e
Chinnari Chongala <chongalachinnari.marolix@gamil.com>
2023-08-03 10:37:18 +0530

    solution

--- appendingthestring.py
+++ appendingthestring.py
@@ -1,15 +1,18 @@
-string = input("enter the string:")
+string = input("enter the string:").split()
 updated_string = input("enter the string")
 length = len(string)
+string = list(string)
 # if length %2 == 0:
 #     result = string[length/2]
 #     string[result] = "chinnari"
 # else:
 #     result = string(length)+1/2
 if length %2 == 0:
-    string[length/2] = updated_string
+    
+    string[int(length/2)] = updated_string
     print(string)
 else:
-    half = (length+1)/2
+
+    half = int(length+1)/2
     string[half] = updated_string 
     print(string) 
